400c1c215d350eb5922932e9d4fcb7711d08f32a
roninhaf <roninhaf@gmail.com>
2019-03-06 15:56:03 -0500

    Improved reading

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+"use strict";
+
 const infosource = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
         const cities = [];
 
         fetch(infosource)
-        .then(a => a.json())
-        .then(a => cities.push(...a));
+        .then(dataReturned => dataReturned.json())
+        .then(data => cities.push(...data));
 
         function matchWord(wordToMatch, cities) {
 
             const word = new RegExp(wordToMatch, 'gi');
-            return cities.filter(function (a) {
+            return cities.filter(function (place) {
 
-                return a.city.match(word) || a.state.match(word);
+                return place.city.match(word) || place.state.match(word);
             })
         }
 
@@ -19,21 +21,21 @@ const infosource = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420
 
         search.addEventListener('keyup', displayMatches);
 
-        function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        function numberWithCommas(num) {
+        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
         }
 
         function displayMatches(val) {
             const matchArray = matchWord(val.target.value, cities);
-            const html = matchArray.map(a => {
+            const html = matchArray.map(place => {
 
                 const regex = new RegExp(val.target.value, 'gi');
-                const cityName = a.city.replace(regex, `<span class="hl">${val.target.value}</span>`);
-                const stateName = a.state.replace(regex, `<span class="hl">${val.target.value}</span>`);
+                const cityName = place.city.replace(regex, `<span class="hl">${val.target.value}</span>`);
+                const stateName = place.state.replace(regex, `<span class="hl">${val.target.value}</span>`);
 
                 return `<li>
                             <span class ="name">${cityName} ${stateName}</span>
-                            <span class="population">${numberWithCommas(a.population)}</span>
+                            <span class="population">${numberWithCommas(place.population)}</span>
                         </li>`;
                 }).join('');
             suggestions.innerHTML = html;
